5f1f6afa8c6c98a56ee5632b8919608d66486495
Daniel Kirushin <retrostyae@mail.ru>
2024-07-23 17:33:26 +0300

    Add:ModalBasket

--- my-app/src/cmps/AppHeader.tsx
+++ my-app/src/cmps/AppHeader.tsx
@@ -1,8 +1,9 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { Search } from "./Search";
 import { Svg } from "./Svg";
+import { BasketModal } from "./BasketModal";
 
 interface AppHeaderProps {
   onSearch: (query: string) => void;
@@ -10,24 +11,29 @@ interface AppHeaderProps {
 
 export function AppHeader({ onSearch }: AppHeaderProps) {
   const count = useSelector((state: RootState) => state.cart.counter);
+  const [showModal, setShowModal] = useState(false); // Состояние для управления видимостью модального окна
+
+  const getModal = () => {
+    setShowModal(!showModal); // Переключение состояния модального окна
+  };
 
   return (
     <section className="app-header">
       <div className="logo">
         <h1>Here будет Logo</h1>
       </div>
-     
 
-      <Search onSearch = {onSearch} />
+      <Search onSearch={onSearch} />
 
-      <div className="options">
+      <button onClick={getModal} className="options">
         <div className="price-basket">
           <p>Корзина</p>
         </div>
         <div className="basket">
           <p>{count}</p>
         </div>
-      </div>
+      </button>
+      {showModal && <BasketModal onClose={getModal} />} {/* Рендер модального окна при открытии */}
     </section>
   );
 }
---
+++ my-app/src/cmps/BasketModal.tsx
@@ -0,0 +1,31 @@
+
+
+interface AddModalProps {
+  onClose: () => void;
+}
+
+// Компонент модального окна корзины
+export function BasketModal({ onClose }: AddModalProps) {
+  return (
+    // Задний фон модального окна
+    <div className="basket-modal-backdrop" onClick={onClose}>
+      {/* Секция самого модального окна */}
+      <section className="basket-modal" onClick={(e) => e.stopPropagation()}>
+        <header>
+          <p>Ваши заказы</p>
+          <p>Общая сумма заказа:</p>
+        </header>
+        <div className="content-basket">
+          <div className="render-order">
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+          <button>Заказать</button>
+          <div className="order-info"></div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
